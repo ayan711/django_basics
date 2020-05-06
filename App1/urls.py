4eafc36a180605e69dbadd84da9fac7fe91da7fe
ayan711 <ayanusmani711@gmail.com>
2020-05-07 00:08:51 +0530

    Added Dictionary

--- App1/urls.py
+++ App1/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('user/', views.user),
-    path('login/', views.login)
+    path('login/', views.login),
+    path('about/',views.about)
 ]
--- App1/views.py
+++ App1/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+information = [
+    {
+        'Dev_Info':'About Developer',
+        'created_by': 'Ayan',
+        'hosted_on': 'Ubuntu'
+    }
+     ,
+    {
+        'Dev_Info':'About Developer',
+        'created_by': 'Unknown',
+        'hosted_on': 'Ubuntu'
+    }
+]
 def home(request):
     return HttpResponse('### This is Home Page of App1 ###')
 def user(request):
     return HttpResponse('***This is the User Page of App1***')
 def login(request):
     return render(request, 'App1/page.html')
+def about(request):
+   context = {
+       'info':information
+   }
+   return render(request,'App1/about.html',context)
